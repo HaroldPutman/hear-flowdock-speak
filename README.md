83a04e36f4396ddd728bdaefe4231f2c82bc4988
Harold Putman <harold.putman@lexmark.com>
2017-02-16 11:04:13 -0500

    Improved configuration
    * Put the available voices into config file, and remove OS-specific code.
    * Improve the grammar of the welcome message.

--- README.md
+++ README.md
@@ -4,16 +4,27 @@ A commandline utility to vocalize a Flowdock chat stream.
 
 ## Configuration
 
+### Environment Variables
+
+Set your Flowdock personal API token in an environment variable
+`FLOWDOCK_API_KEY`. You can find this Token by logging in to Flowdock
+(or CA Agile Central) and visiting [https://www.flowdock.com/account/tokens](https://www.flowdock.com/account/tokens).
+
 ### config.json
+
 You must set up configuration by copying `config.template.json` to `config.json`
-and editting to match your environment.
+and editing to match your environment.
 
 * `organization` - Your organization name in flowdock
 * `flowdockApiKey` - _Optional_ Your Flowdock API key. You can set this here or
 in the environment variable.
-* `flows` - an array of names of flows to vocalize
-* `voices` - A map of Flowdock user names to Mac OS X Voices. If a user is not in the list, they are assigned a random voice.
-* `filters` - A collection of regular expressions to improve pronunciations.
+* `flows` - an array of names of flows to vocalize.
+* `voices` - an array of names of the available voices for your platform. See the [say](https://www.npmjs.com/package/request) package for details.
+* `userVoices` - A map of Flowdock user names to OS Voices. If a user is not in the list, they are assigned a random voice.
+* `filters` - A collection of regular expressions to improve pronunciations. This is an array of objects with these properties:
+  * `match`- The regular expression to match (remember to escape backslashes)
+  * `flags` - Optional regex flags. The default is `gi`
+  * `say` - The replacement text. Use `$1` to refer to first capture group.
 * `defaultRequestOptions` - the defaultRequestOptions is an object passed to
 the underlying [request](https://www.npmjs.com/package/request) module to use
 as default options (ie. proxy settings).
--- index.js
+++ index.js
@@ -3,38 +3,25 @@
 const FlowdockStream = require('flowdock-stream');
 const Say = require('say');
 const config = require('./config.json');
-var apikey = process.env.FLOWDOCK_API_KEY ? process.env.FLOWDOCK_API_KEY : config.flowdockApiKey;
+const apikey = process.env.FLOWDOCK_API_KEY ? process.env.FLOWDOCK_API_KEY : config.flowdockApiKey;
 if (!apikey) {
   console.error('Flowdock API key not set. Check your configuration.');
 }
-var flowdockStream = FlowdockStream.createClient(
+const flowdockStream = FlowdockStream.createClient(
   config.organization,
   config.flows,
   apikey,
   config.defaultRequestOptions);
 
-var voices = [''];
-
-if (process.platform === 'linux') {
-  // Assumes voices installed for Festival (http://www.festvox.org/)
-  voices = ['voice_rab_diphone', 'voice_ked_diphone', 'voice_kal_diphone',
-    'voice_don_diphone', 'voice_en1_mbrola', 'voice_us1_mbrola',
-    'voice_us2_mbrola', 'voice_us3_mbrola'];
-} else if (process.platform == 'darwin') {
-  // MacOS
-  voices = ['Alex', 'Agnes', 'Bruce', 'Kathy',
-    'Junior', 'Princess', 'Vicki', 'Ralph',
-    'Victoria', 'Fred', 'Albert', 'Whisper'];
-}
-// Windows [ignores the voice parameter](https://github.com/Marak/say.js/issues/46).
+const voices = config.voices;
 
 
-var speakingVoices = config.voices;
+const speakingVoices = config.userVoices;
 
-var lastSpoke = {};
+const lastSpoke = {};
 var lastSpeaker = '';
 
-var filters = config.filters;
+const filters = config.filters;
 // Precompile all the filter regular expressions
 for (let i = 0; i < filters.length; i++) {
   let flags = 'flags' in filters[i] ? filters[i].flags : 'gi';
@@ -43,30 +30,34 @@ for (let i = 0; i < filters.length; i++) {
 
 flowdockStream.on('ready', function onReady() {
   let flows = '';
-  for (let id in flowdockStream.flows) {
-    flows += ` ${flowdockStream.flows[id].name}`;
+  let keys = Object.keys(flowdockStream.flows);
+  for (let i = 0; i < keys.length; i++) {
+    if (i > 0) {
+      flows += (keys.length - i == 1) ? ' and' : ',';
+    }
+    flows += ` ${flowdockStream.flows[keys[i]].name}`;
   }
   Say.speak(`Connected to ${flows}.`);
   console.log(`Connected to ${flows}.`);
 });
 
 flowdockStream.on('data', function flowDockEventHandler(data) {
-  var sourceFlow = flowdockStream.flows[data.flow];
+  let sourceFlow = flowdockStream.flows[data.flow];
   if (data.event === 'message') {
-    var prolog = '';
-    var from = (data.user) ? sourceFlow.users[data.user] : null;
+    let prolog = '';
+    let from = (data.user) ? sourceFlow.users[data.user] : null;
     if (from != lastSpeaker) {
       lastSpeaker = from;
       if (!(from in speakingVoices)) {
         speakingVoices[lastSpeaker] = voices[Math.floor(Math.random() * voices.length)];
       }
-      var now = Date.now() / 1000;
+      let now = Date.now() / 1000;
       if (!(from in lastSpoke) || (now - lastSpoke[from] > 300)) {
         lastSpoke[from] = now;
         prolog = `This is ${from}, `;
       }
     }
-    var msg = data.content;
+    let msg = data.content;
     for (let i = 0; i < filters.length; i++) {
       msg = msg.replace(filters[i].re, filters[i].say);
     }
